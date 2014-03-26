e354b25e050447861e021ff9c02d3e8f01d664ea
claudio rocchini <claudio@rockini.name>
2014-03-26 14:25:42 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/sample/bundle/mythirdbundle/locale/it.js
@@ -0,0 +1,12 @@
+Oskari.registerLocalization({
+  "lang": "it",
+  "key": "MyThirdBundle",
+  "value": {
+    "title": "Ciao mamma",
+    "flyouttitle": "Ciao",
+    "desc": "",
+    "flyout": {
+      "sayHello": "Ciao mamma!"
+    }
+  }
+});
