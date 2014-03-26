1e5e17860b73a4185d273f83f5467d340b5c83d3
claudio rocchini <claudio@rockini.name>
2014-03-26 14:33:06 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/admin-layerrights/locale/it.js
@@ -0,0 +1,24 @@
+Oskari.registerLocalization({
+    "lang" : "it",
+    "key" : "admin-layerrights",
+    "value" : {
+        "title" : "Diritti sul layer",  
+        "desc": "Descrizione",      
+        "tile" : {
+            "title" : "Diritti sul layer"
+        },
+        "flyout" : {
+            "title" : "Gestione diritti",
+            "message" : "Gestione diritti"
+        },
+        "rights" : {
+            "name"              : "Nome",
+            "rightToPublish"    : "Diritti di pubblicazione",
+            "rightToView"       : "Diritti di visualizzazione",
+            "rightToDownload"   : "Diritti di download",
+            "rightToPublishView": "Diritti di visualizzazione nelle mappe embedded"
+        },
+        "save" : "Salva",
+        "selectRole" : "Ruolo"
+    }
+});
