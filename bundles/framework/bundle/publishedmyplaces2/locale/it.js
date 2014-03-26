1b70d2661df18f6f7b7dd57c63265f988c978419
claudio rocchini <claudio@rockini.name>
2014-03-26 14:11:14 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/publishedmyplaces2/locale/it.js
@@ -0,0 +1,325 @@
+Oskari.registerLocalization({
+  "lang": "en",
+  "key": "PublishedMyPlaces",
+  "value": {
+    "title": "Luoghi",
+    "desc": "",
+    "category": {
+      "defaultName": "I miei layer",
+      "organization": "I miei luoghi",
+      "inspire": "I miei luoghi"
+    },
+    "guest": {
+      "loginShort": "Fai login per usare questa funzionalita'"
+    },
+    "tab": {
+        "title": "Luoghi",
+        "nocategories": "Non hai salvato i luoghi.",
+        "publishCategory": {
+            "privateTooltip": "Il layer e' privato. Cliccaci sopra per renderlo pubblico.",
+            "publicTooltip": "Il layer e' pubblico. Cliccaci sopra per renderlo privato."
+        },
+        "addCategory": "Aggiungi layer",
+        "editCategory": "Modifica layer",
+        "deleteCategory": "Cancella layer",
+        "edit": "Modifica",
+        "delete": "Cancella",
+        "grid": {
+          "name": "Nome",
+          "desc": "Descrizione",
+          "createDate": "Data di creazione",
+          "updateDate": "Data di modifica",
+          "measurement": "Lunghezza/area",
+          "edit": " ",
+          "delete": " "
+        },
+        "notification": {
+          "delete": {
+            "title": "Cancella luogo",
+            "confirm": "Vuoi cancellare questo luogo?",
+            "btnDelete": "Cancella",
+            "btnCancel": "Annulla",
+            "success": "Il luogo e' stato cancellato.",
+            "error": "Il luogo non puo' essere cancellato; riprova piu' tardi.",
+            "cancel": "L'oggeto non puo' essere cancellato."
+          }
+        }
+    },
+    "tools": {
+      "measureline": {
+        "title": "Misura della distanza",
+        "tooltip": "Misura della distanza",
+        "add": "",
+        "next": "",
+        "edit": "Modifica la linea cliccando sui suoi nodi.",
+        "noResult": "0 m"
+      },
+      "measurearea": {
+        "title": "Misura dell'area",
+        "tooltip": "Misura dell'area",
+        "add": "",
+        "next": "",
+        "edit": "Modifica l'area trascinandone i nodi.",
+        "noResult": "0 m²"
+      },
+      "point": {
+        "title": "Aggiungi punto",
+        "tooltip": "Aggiungi punto",
+        "add": "Aggiungi un punto cliccando sulla mappa.",
+        "next": "Puoi disegnare e salvare piu' punti nello stesso disegno.",
+        "edit": "Sposta i punti trascinadoli.",
+        "save": "Salva luogo"
+      },
+      "line": {
+        "title": "Aggiungi linea",
+        "tooltip": "Aggiungi linea",
+        "add": "Aggiungi nodi della linea cliccando sulla mappa. Ferma il disegno facendo doppio click o scegliendo 'Fine disegno'.",
+        "next": "Puoi salvare e disegnare piu' linee nello stesso disegno.",
+        "edit": "Modifica la linea cliccando sui nodi e trascinadoli.",
+        "save": "Salva la linea",
+        "noResult": "0 m"
+      },
+      "area": {
+        "title": "Aggiungi area",
+        "tooltip": "Aggiungi area",
+        "add": "Aggiungi nodi del poligono cliccando sulla mappa. Ferma il disegno facendo doppio click o selezionando 'Fine disegno'. Per creare buchi, tieni premuto il tasto Alt.",
+        "next": "Puoi salvare e disegnare piu' poligoni nello stesso disegno.",
+        "edit": "Modifica la forma del poligono cliccando e trascinando i nodi.",
+        "save": "Salva il poligono",
+        "noResult": "0 m²"
+      }
+    },
+    "buttons": {
+      "ok": "OK",
+      "cancel": "Annulla",
+      "close": "Chiudi",
+      "finish": "Salva il disegno",
+      "save": "Salva",
+      "saveAsMyPlace": "Salva come Mio Luogo",
+      "movePlaces": "Sposta e cancella i luoghi",
+      "deleteCategory": "Cancella",
+      "deleteCategoryAndPlaces": "Cancella categorie e luoghi",
+      "changeToPublic": "Rendi pubblico",
+      "changeToPrivate": "Rendi privato"
+    },
+    "placeform": {
+      "title": "Dati dei luoghi",
+      "tooltip": "Un luogo della mappa puo' essere memorizzato in Miei Luoghi. Dai un nome ed una descrizione al luogo. Puoi salvare il luogo in un layer esistente o crearne uno nuovo selezionando 'Nuovo Layer' nel menu' dei layer.",
+      "placename": {
+        "placeholder": "Battezza il luogo"
+      },
+      "placelink": {
+        "placeholder": "URL"
+      },
+      "placedesc": {
+        "placeholder": "Descrivi il luogo"
+      },
+      "placeAttention": {
+            "placeholder": "Label"
+      },
+      "measurement": {
+        "line": "Lunghezza: ",
+        "area": "Area: "
+      },
+      "category": {
+        "label": "Layer",
+        "newLayer": "Crea un nuovo layer",
+        "choose": " o selezionane uno esistente:"
+      },
+      "imagelink": {
+        "placeholder": "URL dell'immagine",
+        "previewLabel": "Ppreview immagine",
+        "add": "Nuovo layer..."
+      },
+      "rendering": {
+        "label": "Stile",
+        "point": {
+          "tooltip": "Punto"
+        }
+      }
+    },
+    "categoryform": {
+      "name": {
+        "label": "Nome",
+        "placeholder": "Battezza il layer"
+      },
+      "drawing": {
+        "label": "Stile",
+        "point": {
+          "label": "Punto",
+          "color": "Colore",
+          "size": "Dimensione"
+        },
+        "line": {
+          "label": "Linea",
+          "color": "Colore",
+          "size": "Spessore"
+        },
+        "area": {
+          "label": "Area",
+          "fillcolor": "Colore di riempimento",
+          "linecolor": "Colore del bordo",
+          "size": "Spessore del bordo"
+        }
+      },
+      "visibleFields": {
+        "label": "Campi visibili",
+        "placename": "Nome",
+        "placedesc": "Descrizione",
+        "image": "Immagine"
+      },
+      "edit": {
+        "title": "Modifica layer",
+        "save": "Salva",
+        "cancel": "Annulla"
+      },
+      "rendering": {
+        "label": "Stile delle feature",
+        "point": {
+          "tooltip": "Modifica stile del punto"
+        },
+        "area": {
+          "tooltip": "Modifica stile dell'area"
+        },
+        "line": {
+          "tooltip": "Modifica stile della linea"
+        }
+      }
+    },
+    "pointform": {
+      "title": "Rappresentazione dei punti",
+      "tooltip": "",
+      "symbol": {
+        "label": "Icona del punto"
+      },
+      "size": {
+        "label": "Dimensione"
+      },
+      "color": {
+        "label": "Colore",
+        "labelOr": "o",
+        "labelCustom": "Colore RGB personalizzato (0-255)"
+      },
+      "preview": {
+        "label": "Preview"
+      },
+      "buttons": {
+        "save": "Salva",
+        "cancel": "Annulla"
+      }
+    },
+    "lineform": {
+      "title": "Rappresentazione delle linee",
+      "tooltip": "",
+      "style": {
+        "label": "Stile linea"
+      },
+      "cap": {
+        "label": "Estremi della linea"
+      },
+      "corner": {
+        "label": "Angoli"
+      },
+      "width": {
+        "label": "Spessore"
+      },
+      "color": {
+        "label": "Colore",
+        "labelOr": "o",
+        "labelCustom": "Colore RGB personalizzato (0-255)"
+      },
+      "preview": {
+        "label": "Preview"
+      },
+      "buttons": {
+        "save": "Salva",
+        "cancel": "Annulla"
+      }
+    },
+    "areaform": {
+      "title": "Rappresentazione delle aree",
+      "tooltip": "",
+      "linestyle": {
+        "label": "Stile linea"
+      },
+      "linecap": {
+        "label": "Estremi del bordo"
+      },
+      "linecorner": {
+        "label": "Angoli"
+      },
+      "linewidth": {
+        "label": "Spessore"
+      },
+      "linecolor": {
+        "label": "Colore bordo",
+        "labelOr": "o",
+        "labelCustom": "Colore RGB personalizzato (0-255)"
+      },
+      "color": {
+        "label": "Colore di riempimento",
+        "labelOr": "o",
+        "labelCustom": "Colore RGB personalizzato (0-255)"
+      },
+      "fill": {
+        "label": "Pattern di riempimento"
+      },
+      "preview": {
+        "label": "Preview"
+      },
+      "buttons": {
+        "save": "Salva",
+        "cancel": "Annulla"
+      }
+    },
+    "notification": {
+      "placeAdded": {
+        "title": "Il luogo e' stato salvato",
+        "message": "Il luogo puo' essere trovato nel menu' Miei Dati"
+      },
+      "categorySaved": {
+        "title": "Layer salvato",
+        "message": "Modifiche al layer salvato"
+      },
+      "categoryDelete": {
+        "title": "Cancella layer",
+        "deleteConfirmMove": "Il layer {0} contiene {1} oggetti. Vuoi cancellare il layer e spostare gli oggetti contenuti nel layer di default {2} ?",
+        "deleteConfirm": "Cancella il layer {0}?",
+        "deleted": "Layer cancellato"
+      },
+      "categoryToPublic": {
+        "title": "Rendi pubblico il layer",        
+        "message": "Stai per rendere pubblico il layer \"{0}\". Puoi condividere il link ad un layer pubblico con gli altri utentei, oopure inserirlo in una mappa di un altro sito. Gli altri utenti potranno visualizzare il layer su Paikkatietoikkuna."
+      },
+      "categoryToPrivate": {
+        "title": "Rendi privato il layer",        
+        "message": "Stai per rendere privato il layer \"{0}\". Dopo cio' non sara' piu' possibile condividerlo con gli altri utenti o inserirlo in un altro sito. Gli altri utenti non potranno piu' visualizzarlo su Paikkatietoikkuna."
+      },
+      "error": {
+        "addCategory": "Il layer non puo' essere salvato. I luoghi non sono stati salvati.",
+        "editCategory": "Il layer non puo' essere salvato.",
+        "savePlace": "Il luogo non puo' essere salvato.",
+        "title": "Errore!",
+        "generic": "Errore di sistema; riprova fra un attimo.",
+        "deleteCategory": "Errore durante la cancellazione!",
+        "deleteDefault": "Il layer di default non puo' essere cancellato."
+      }
+    },
+    "validation": {
+      "title": "I dati contengono alcuni errori:",
+      "placeName": "Manca il nome del luogo.",
+      "categoryName": "Manca il nome del layer.",
+      "placeNameIllegal": "Il nome dell'oggetto contiene caratteri non validi. I caratteri accettati sono le lettere, le cifre, il sottolineato ed il trattino.",
+      "descIllegal": "La descrizione dell'oggetto contiene caratteri non validi. I caratteri accettati sono le lettere, le cifre, il sottolineato ed il trattino.",
+      "categoryNameIllegal": "La descrizione del layer contiene caratteri non validi. I caratteri accettati sono le lettere, le cifre, il sottolineato ed il trattino.",
+      "dotSize": "La dimensione del punto non e' compresa nell'intervallo consentito (1-5).",
+      "dotColor": "Colore del punto errato.",
+      "dotShape": "Forma del punto errata.",
+      "lineSize": "Lo spessore della linea non e' compreso nell'intervallo consentito (1-50).",
+      "lineColor": "Colore della linea errato.",
+      "areaLineSize": "Lo spessore dalla linea di bordo non e' compreso nell'intervallo consentito (0-50).",
+      "areaLineColor": "Colore del bordo errato.",
+      "areaFillColor": "Colore di riempimento errato."
+    }
+  }
+});
