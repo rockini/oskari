3f25cf007725da7407c3518b07a7ae14adf13ff1
claudio rocchini <claudio@rockini.name>
2014-03-26 14:33:54 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/backendstatus/locale/it.js
@@ -0,0 +1,23 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "BackendStatus",
+	"value" : {
+		"title" : "Stato del Backend",
+		"desc" : "",
+		"feedback" : {
+			"missing_backendstatus_status" : {
+				"title" : "Layer",
+				"message" :"Le informazioni sullo stato di salute del servizio layer non sono disponibili"	
+			},
+			"missing_backendstatus_information" : {
+				"title" : "layer",
+				"message" :"Le informazioni sullo stato di salute di questo layer non sono disponibili"	
+			},
+			"missing_backendstatus_infourl" : {
+				"title" : "layer",
+				"message" :"Le informazioni sullo stato di salute di questo layer non sono disponibili"	
+			}
+		}
+
+	}
+});
