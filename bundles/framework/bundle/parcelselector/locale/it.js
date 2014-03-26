63864e0806b7e1a569e128f2c07b1ac819378d98
claudio rocchini <claudio@rockini.name>
2014-03-26 14:08:03 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/parcelselector/locale/it.js
@@ -0,0 +1,20 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "ParcelSelector",
+	"value" : {
+		"title" : "Colonna di ricerca",
+		"button" : "Cerca",
+		"errors" : {
+            "title": "Errore!",
+			"illegalInput" : "Il tag deve essere un numero."
+		},
+		"filter" : {
+			"text" : "ID della proprieta'",
+			"parcel" : "Colonna",
+			"registerUnit" : "Unita' di registrazione"
+		},
+		"buttons": {
+            "ok": "OK"
+        }
+	}
+});
