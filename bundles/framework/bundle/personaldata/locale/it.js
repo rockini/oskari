6d447a5f6e45e75d3a10062444b1994ef0e9f4d8
claudio rocchini <claudio@rockini.name>
2014-03-26 14:08:54 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/personaldata/locale/it.js
@@ -0,0 +1,132 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "PersonalData",
+    "value": {
+        "title": "I miei dati",
+        "desc": "I miei dati",
+        "notLoggedIn": "Puoi salvare viste e mappe embedded in Miei Dati. <a href='/web/en/login'>Login</a>.",
+        "tabs": {
+            "myviews": {
+                "title": "Le mie viste",
+                "edit": "Modifica",
+                "publish": "Pubblica",
+                "unpublish": "Annulla pubblicazione",
+                "delete": "Cancella",
+                "grid": {
+                    "name": "Nome",
+                    "description": "Descrizione",
+                    "publish": "Pubblicata",
+                    "edit": " ",
+                    "delete": " "
+                },
+                "popup": {
+                    "edit": "Modifica i dati della vista",
+                    "title": "Salva la vista",
+                    "label": "Nome della vista:",
+                    "name_placeholder": "Battezza la vista",
+                    "description_placeholder": "Scrivi una descrizione della vista",
+                    "save": "Salva",
+                    "cancel": "Annulla",
+                    "error_noname": "Digita il nome",
+                    "deletetitle": "Conferma la cancellazione",
+                    "deletemsg": "Sei sicuro di voler cancellare la vista?"
+                },
+                "save": {
+                    "success": "La vista e' stata salvata.",
+                    "error_noname": "Il campo nome non puo' essere vuoto.",
+                    "error_illegalchars": "Il nome contiene caratteri non validi. I caratteri accettati sono le lettere, le cifre, il sottolineato ed il trattino.",
+                    "msg": {
+                        "view_name": "Il nome della vista"
+                    }
+                },
+                "button": {
+                    "ok": "OK",
+                    "save": "Salva",
+                    "cancel": "Annulla",
+                    "toolbarsave": "Salva la vista"
+                },
+                "error": {
+                    "title": "Errore!",
+                    "generic": "Errore di sistema; riprova piu' tardi.",
+                    "notsaved": "La vista non puo' essere salvata.",
+                    "notdeleted": "La vista non puo' essere cancellata.",
+                    "makePublic": "La vista non puo' essere pubblicata; riprova piu' tardi.",
+                    "makePrivate": "La pubblicazione non puo' essere annullata; riprova piu' tardi.",
+                    "loadfailed": "La vista non puo' essere scaricata."
+                }
+            },
+            "publishedmaps": {
+                "title": "Mappe ebedded",
+                "show": "Mostra la mappa",
+                "getHTML": "Codice HTML",
+                "edit": "Modifica",
+                "publish": "Pubblica mappa",
+                "unpublish": "Annulla la pubblicazione della mappa",
+                "delete": "Cancella",
+                "grid": {
+                    "name": "Nome",
+                    "domain": "Impostazione della mappa",
+                    "publish": "Stato",
+                    "show": "Azioni",
+                    "html": " ",
+                    "edit": " ",
+                    "delete": " "
+                },
+                "published": {
+                    "desc": "Incorpora la mappa aggiungendo il sottoscritto codice HTML direttamente nel tuo sito."
+                },
+                "popup": {
+                    "edit": "Cambia il nome della mappa",
+                    "title": "Salva la mappa",
+                    "label": "Nome della mappa",
+                    "placeholder": "Battezza la mappa",
+                    "save": "Salva",
+                    "cancel": "Annulla",
+                    "error_noname": "Battezza la mappa!",
+                    "deletetitle": "Cancella la mappa?",
+                    "deletemsg": "Vuoi cancellare la mappa embeddded? Non sara' piu' visibile sul tuo sito web.",
+                    "showErrorTitle": "Vista incompleta",
+                    "showConfirmMissing": "Alcuni layer della mappa non sono disponibili; vuoi comunque aprire questa vista?",
+                    "showConfirmNotLoaded": "Alcuni layer della mappa non sono stati caricati; riprova piu' tardi."
+                },
+                "save": {
+                    "success": "La mappa e' stata salvata.",
+                    "error_noname": "Il campo nome non puo' essere vuoto.",
+                    "error_illegalchars": "Il nome contiene caratteri non consentiti. I caratteri consentiti sono le lettere,le cifre, il sottolineato ed il trattino.",
+                    "msg": {
+                        "view_name": "Nome della mappa"
+                    }
+                },
+                "button": {
+                    "ok": "OK",
+                    "save": "Salva",
+                    "cancel": "Annulla",
+                    "toolbarsave": "Salva la mappa"
+                },
+                "error": {
+                    "title": "Errore!",
+                    "generic": "Errore di sistema; riprova piu' tardi.",
+                    "notsaved": "La mappa non puo' essere salvata.",
+                    "notdeleted": "La mappa non puo' essere cancellata.",
+                    "makePublic": "La mappa non puo' essere pubblicata; riprova piu' tardi.",
+                    "makePrivate": "La pubblicazione non puo' essere annullata; riprova piu' tardi."
+                }
+            },
+            "account": {
+                "title": "Account",
+                "firstName": "Nome",
+                "lastName": "Cognome",
+                "nickName": "Nickname",
+                "email": "E-mail",
+                "changeInfo": "Cambia le informazioni del tuo account",
+                "changePassword": "Cambia la password",
+                "removeAccount": "Cancella il tuo account",
+                "changeInfoUrl": "/web/en/profile"
+            }
+        },
+        "button": {
+            "yes": "Si",
+            "no": "No"
+        }
+    }
+});
