ffe1fcbb64c2e6228617f79c76c551e1912fec72
claudio rocchini <claudio@rockini.name>
2014-03-26 11:42:56 +0100

    Create it.js
    it translation from Istituto Geografico Militare

---
+++ bundles/catalogue/bundle/metadataflyout/locale/it.js
@@ -0,0 +1,80 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "catalogue.bundle.metadataflyout",
+    "value": {
+        "title": "Metadati",
+        "desc": "",
+        "loader": {
+            "json": "/catalogue/portti-metadata-printout-service/MetadataServlet?",
+            "abstract": "/geonetwork/srv/en/metadata.show.portti.abstract?",
+            "inspire": "/geonetwork/srv/en/metadata.show.portti?",
+            "jhs": "/geonetwork/srv/en/metadata.show.portti.jhs158?",
+            "pdf": "/catalogue/portti-metadata-printout-service/MetadataPrintoutServlet?lang=sv&title=METADATAPRINTOUT&metadataresource",
+            "xml": "/geonetwork/srv/en/iso19139.xml?",
+            "schemas": "/geonetwork/srv/en/metadata.show.portti.skeemat?"
+        },
+        "layer": {
+            "name": "Metadati",
+            "description": "",
+            "orgName": "Metadati",
+            "inspire": "Metadati"
+        },
+        "flyout": {
+            "title": "Metadati",
+            "abstract": "Abstract",
+            "inspire": "INSPIRE",
+            "jhs": "JHS",
+            "xml": "XML",
+            "map": "Estensione Geografica",
+            "pdf": "Uscita di stampa",
+            "select_metadata_prompt": "Seleziona i metadati cliccando sulle icone.",
+            "metadata_printout_title": "METADATA PRINTOUT",
+            "linkto": "Collegamento a questo metadato",
+            "tabs": {
+                "abstract": {
+                    "abstract": "",
+                    "jhs": "Mostra JHS",
+                    "inspire": "Mostra INSPIRE",
+                    "xml": {
+                        "text": "Apri ISO 19139 XML",
+                        "target": "_blank"
+                    },
+                    "pdf": {
+                        "text": "Apri PDF-printout",
+                        "target": "_blank"
+                    }
+                },
+                "jhs": {
+                    "abstract": "Mostra abstract",
+                    "jhs": "",
+                    "inspire": "Mostra INSPIRE",
+                    "xml": {
+                        "text": "Apri ISO 19139 XML",
+                        "target": "_blank"
+                    },
+                    "pdf": {
+                        "text": "Apri PDF-printout",
+                        "target": "_blank"
+                    }
+                },
+                "inspire": {
+                    "abstract": "Mostra abstract",
+                    "jhs": "Mostra JHS",
+                    "inspire": "",
+                    "xml": {
+                        "text": "Apri ISO 19139 XML",
+                        "target": "_blank"
+                    },
+                    "pdf": {
+                        "text": "Apri PDF-printout",
+                        "target": "_blank"
+                    }
+                }
+            }
+        },
+        "tile": {
+            "title": "Metadati",
+            "tooltip": "I metadati contengono le informazioni circa la disponibilita' dei dati."
+        }
+    }
+});
