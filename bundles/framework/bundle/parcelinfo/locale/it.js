74a5493e5428214c2f075c271d167a775a701ae5
claudio rocchini <claudio@rockini.name>
2014-03-26 14:07:25 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/parcelinfo/locale/it.js
@@ -0,0 +1,16 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "parcelinfo",
+	"value" : {
+		"title" : "",
+		"desc" : "",
+		"display" : {
+		    "header" : "Dati selezionati",
+			"info" : {
+			    "name" : "Nome: ",
+				"area" : "Area: ",
+				"length" : "Lunghezza: "
+			}
+		}
+	}
+});
