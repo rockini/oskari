2a62812608fb9c123f9fa774c142af91947beca2
claudio rocchini <claudio@rockini.name>
2014-03-26 14:16:54 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/userguide/locale/it.js
@@ -0,0 +1,22 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "userinterface.UserGuide",
+    "value": {
+        "title": "Aiuto",
+        "desc": "",
+        "flyout": {
+            "title": "Guida all'uso di Paikkatietoikkuna"
+        },
+        "tile": {
+            "title": "Guida utente della mappa"
+        },
+        "error": {
+            "title": "Errore!",
+            "generic": "Caricamento della guida utente fallito; riprova piu' tardi."
+        },
+        "help": {
+            "tags": "finestra di mappa,guida utente",
+            "contentPart": "body"
+        }
+    }
+});
