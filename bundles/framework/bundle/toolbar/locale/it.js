d4fb0766ccc0ebdd854f6e7a071bf4a4b54ac1dc
claudio rocchini <claudio@rockini.name>
2014-03-26 14:15:48 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/toolbar/locale/it.js
@@ -0,0 +1,35 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "Toolbar",
+    "value": {
+        "buttons": {
+            "link": {
+                "tooltip": "Link",
+                "ok": "Ok",
+                "title": "Collega alla vista"
+            },
+            "print": {
+                "tooltip": "Stampa"
+            },
+            "history": {
+                "reset": "Ritorna alla vista di default",
+                "back": "Indietro",
+                "next": "Avanti"
+            },
+            "pan": "Sposta la vista",
+            "zoom": "Zoom avanti",
+            "measure": {
+                "line": "Misura distanza",
+                "area": "Misura area"
+            }
+        },
+        "measure": {
+            "title": "Misure",
+            "close": "Annulla",
+            "guidance": {
+                "measureline": "Misura una distanza sulla mappa. Doppio click per finire.",
+                "measurearea": "Misura un'area sulla mappa. Doppio click per finire."
+            }
+        }
+    }
+});
