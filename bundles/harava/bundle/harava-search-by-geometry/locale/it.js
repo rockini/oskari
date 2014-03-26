6362a81bcd1f90102e5a07dcf5b7766d49ac6bab
claudio rocchini <claudio@rockini.name>
2014-03-26 14:19:59 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/harava/bundle/harava-search-by-geometry/locale/it.js
@@ -0,0 +1,21 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "HaravaSearchByGeometry",
+	"value" : {
+		"title" : "",
+		"desc" : "",
+		"display" : {
+			"tooltips" : {
+				"searchByPoint": "Cerca con un punto",
+				"searchByLine": "Cerca con una linea",
+				"searchByRegularPolygon": "Cerca con un rettangolo",
+				"searchByPolygon": "Cerca con un poligono",
+				"searchByMapExtent": "Cerca con l'estensione della mappa,",
+				"searchNotFound": "?",
+				"searchNotFoundOkButton": "?",
+				"panMap": "Sposta la mappa",
+				"homeMap": "?"
+			}
+		}
+	}
+});
