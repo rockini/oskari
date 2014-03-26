be651a7d60435f7054193aaef0d8008f15fae599
claudio rocchini <claudio@rockini.name>
2014-03-26 14:21:08 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/integration/bundle/admin-layerselector/locale/it.js
@@ -0,0 +1,189 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "admin-layerselector",
+    "value": {
+        "title": "admin: Layer",
+        "desc": "",
+        "flyout": {
+            "title": "admin: Layer",
+            "fetchingLayers" : "Recupero layer."
+        },
+        "tile": {
+            "title": "A: Layer",
+            "tooltip": "."
+        },
+        "view": {
+            "title": "",
+            "prompt": "",
+            "templates": {}
+        },
+        "errors": {
+            "title": "Errore!",
+            "generic": "Errore di sistema; riprova piu' tardi.",
+            "loadFailed": "Errore durante il caricamento dei layer. Ricarica la pagina e riseleziona il menu' desiderato.",
+            "noResults": "La ricerca non ha ottenuto risultati."
+        },
+        "loading": "Caricamento...",
+        "filter": {
+            "text": "Cerca layer",
+            "inspire": "per tema",
+            "organization": "per fornitore",
+            "published": "Utenti"
+        },
+        "published": {
+            "organization": "Layer pubblicati",
+            "inspire": "layer pubblicati"
+        },
+        "tooltip": {
+            "type-base": "Base",
+            "type-wms": "Layer",
+            "type-wfs": "Prodotti"
+        },
+        "backendStatus": {
+            "OK": {
+                "tooltip": "Il layer e' disponibile.",
+                "iconClass": "backendstatus-ok"
+            },
+            "DOWN": {
+                "tooltip": "Il layer non e' disponibile.",
+                "iconClass": "backendstatus-down"
+            },
+            "MAINTENANCE": {
+                "tooltip": "Nei prossimi giorni si prevedono interruzioni del servizio.",
+                "iconClass": "backendstatus-maintenance"
+            },
+            "UNKNOWN": {
+                "tooltip": "boh",
+                "iconClass": "backendstatus-ok"
+            }
+        },
+        "admin": {
+            "capabilitiesLabel" : "WMSCapabilities",
+            "confirmResourceKeyChange" : "Cambiando il valore di wmsname e dell'URL i permessi verranno resettati. Vuoi continurare?",
+            "confirmDeleteLayerGroup" : "Cancellazione di un layer gruppo. Vuoi continuare?",
+            "confirmDeleteLayer" : "Cancellazione layer. Vuoi continuare?",
+            "layertypes" : {
+                "wms": "WMS layer",
+                "wfs": "WFS layer",
+                "wmts": "WMTS layer"
+            },
+            "selectLayer": "Seleziona un layer",
+            "selectSubLayer": "Seleziona un sotto-layer",
+
+            "addOrganization": "Aggiungi organizzazione",
+            "addOrganizationDesc": "Aggiungi un'organizzazione, cioe' un produttore di dati",
+            "addInspire": "Aggiungi classe",
+            "addInspireDesc": "Aggiungi una classe, ad esempio un tema Inspire",
+            "addLayer": "Aggiungi layer",
+            "addLayerDesc": "Aggiungi un layer di questo tema Inspire",
+            "edit": "Modfica",
+            "editDesc": "Modifica descizione",
+            "layerType": "Tipo di layer",
+            "layerTypeDesc": "Tipo di layer: WMS, WFS, WMTS",
+            "type": "Tipo di layer",
+            "typePlaceholder": "Scegli il tipo del layer",
+            "normalLayer": "Layer normale",
+            "baseLayer": "Layer base",
+            "groupLayer": "Layer gruppo",
+            "interfaceVersion": "Versione dell'interfaccia",
+            "interfaceVersionDesc": "Versione dell'interfaccia",
+            "wms1_1_1": "WMS 1.1.1",
+            "wms1_3_0": "WMS 1.3.0",
+            "getInfo": "Get info",
+            "selectClass": "Seleziona classe",
+            "selectClassDesc": "Selezioan tema inspire",
+
+            "baseName": "Nome del layer di base",
+            "groupName": "Nome del layer gruppo",
+            "subLayers": "Sott-layer",
+            "addSubLayer": "Aggiungi sotto-layer",
+
+            "wmsInterfaceAddress": "URL del WMS",
+            "wmsUrl": "URL del WMS",
+            "wmsInterfaceAddressDesc": "Indirizzi dei servizi WMS, separati da virgola",
+            "wmsServiceMetaId": "ID del metadato WMS",
+            "wmsServiceMetaIdDesc": "ID del metadato WMS",
+            "layerNameAndDesc": "Nome e descrizione del layer",
+
+            "metaInfoIdDesc": "L'ID del metadato che indentifica la descrizione XML di questo metadato",
+            "metaInfoId": "ID del metadato",
+            "wmsName": "Nome del WMS",
+            "wmsNameDesc": "Il nome unico del layer WMS",
+
+            "addInspireName": "Nome della classe",
+            "addInspireNameTitle": "Nome della classe Inspire",
+            "addOrganizationName": "Organizzazione",
+            "addOrganizationNameTitle": "Nome dell'organizzazione",
+            "addNewClass": "Aggiungi una nuova classe",
+            "addNewLayer": "Aggiungi un nuovo layer",
+            "addNewGroupLayer": "Aggiungi un nuovo layer gruppo",
+            "addNewBaseLayer": "Aggiungi un nuovo layer di base",
+            "addNewOrganization": "Aggiungi una nuova organizzazione",
+            "addInspireThemes": "Aggiungi classe",
+            "addInspireThemesDesc": "Aggiungi classe (Tema Inspire)",
+            "opacity": "Opacita'",
+            "opacityDesc": "Opacita' del layer (0% = trasparente)",
+            "style": "Stile di default",
+            "styleDesc": "Stile di default",
+
+            "minScale": "Scala minima",
+            "minScaleDesc": "La scala minima del layer",
+            "minScalePlaceholder": "La scala minima del layer",
+            "maxScale": "Scala massima",
+            "maxScaleDesc": "La scala massima del layer",
+            "maxScalePlaceholder": "La scala massima del layer",
+            "srsName": "Sistema di riferimento",
+            "srsNamePlaceholder": "Sistema di riferimento",
+            "legendImage": "Immagine per la legenda",
+            "legendImageDesc": "URL dell'immagine per la legenda",
+            "legendImagePlaceholder": "URL dell'immagine per la legenda",
+            "gfiResponseType": "Tipo di riposta a GFI",
+            "gfiResponseTypeDesc": "Tipo di riposta per il Get Feature Info (GFI)",
+            "gfiStyle": "Stile GFI",
+            "gfiStyleDesc": "Stile GFI (XSLT)",
+            "realtime": "Layer Real time",
+            "refreshRate": "Frequenza di aggiornamento (in secondi)",
+
+            "generic": {
+                "placeholder": "Nome in {0}",
+                "descplaceholder": "Descrizione in {0}"
+            },
+            "en": {
+                "lang": "English:",
+                "title": "En",
+                "placeholder": "Nome in Inglese",
+                "descplaceholder": "Descrizione in English"
+            },
+            "fi": {
+                "lang": "Finnico:",
+                "title": "Fi",
+                "placeholder": "Nome in Finnish",
+                "descplaceholder": "Descrizione in Finnico"
+            },
+            "sv": {
+                "lang": "Svedese:",
+                "title": "Sv",
+                "placeholder": "Nome in Swedish",
+                "descplaceholder": "Descrizione in Svedese"
+            },
+			"it": {
+                "lang": "Italiano:",
+                "title": "It",
+                "placeholder": "Nome in Italiano",
+                "descplaceholder": "Descrizione in Italiano"
+            },
+
+            "interfaceAddress": "URL del servizio",
+            "interfaceAddressDesc": "URL della definzione WMS del layer",
+            "viewingRightsRoles": "Visualizza privilegi",
+            "metadataReadFailure": "Caricamento dei metadati fallito.",
+            "mandatory_field_missing": "Il campo e' obbligatorio: ",
+            "invalid_field_value": "Valore non valido: ",
+            "operation_not_permitted_for_layer_id": "L'operazione non e' permessa sul layer ",
+            "no_layer_with_id": "Il layer non e' stato trovato, cercando con l'id "
+        },
+        "cancel": "Annulla",
+        "add": "Aggiungi",
+        "delete": "Rimuovi"
+    }
+});
