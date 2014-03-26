9c5256ba4756ce5dd8ed68409466aab1fcaef384
claudio rocchini <claudio@rockini.name>
2014-03-26 14:31:36 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/digiroad/bundle/digiroad-personaldata/locale/it.js
@@ -0,0 +1,224 @@
+Oskari.registerLocalization({
+  "lang": "it",
+  "key": "DigiroadPersonalData",
+  "value": {
+    "title": "Miei Dati",
+    "desc": "Miei Dati",
+    "notLoggedIn": "Puoi salvare mappe e viste in Miei Dati. <a href='/web/en/login'>Login</a>.",
+    "tabs": {
+      "myplaces": {
+        "title": "Nuovi segmenti",
+        "restrictionsTitle": "Nuove resitrizioni",
+        "nocategories": "Non hai ancora salvato i luoghi.",
+        "publishCategory": {
+            "privateTooltip": "Il layer e' . Clicca sul layer per renderlo pubblico.",
+            "publicTooltip": "Il layer e' pubblico. Clicca sul layer per renderlo privato."
+        },
+        "editCategory": "Modifica il layer",
+        "deleteCategory": "Cancella il layer",
+        "edit": "Modifica",
+        "delete": "Cancella",
+        "grid": {
+          "name": "Nome",
+          "desc": "Descrizione",
+          "dynType": "Tipo di dato",
+          "dynValue": "Valore",
+          "firstElem": "Primo elemento",
+          "lastElem": "Ultimo elemento",
+          "restrictionType": "Tipo",
+          "createDate": "Data di creazione",
+          "updateDate": "Data di modifica",
+          "edit": " ",
+          "delete": " "
+        },
+        "notification": {
+          "delete": {
+            "title": "Cancella luogo",
+            "confirm": "Vuoi cancellare questo luogo?",
+            "btnDelete": "Cancella",
+            "btnCancel": "Annulla",
+            "success": "Il luogo e' stato cancellato.",
+            "error": "Questo luogo non puo' essere cancellato, prova piu' tardi.",
+            "cancel": "L'oggetto non puo' essere cancellato."
+          }
+        }
+      },
+      "editedfeatures": {
+        "title": "Modifica feature",
+        "nocategories": "Non hai modificato nessuna feature di questo tipo.",
+        "publishCategory": {
+            "privateTooltip": "Il layer e' privato; clicca sulla mappa per renderlo pubblico.",
+            "publicTooltip": "Il layer e' pubblico; clicca sulla mappa per renderlo privato."
+        },
+        "editCategory": "Modifica layer",
+        "deleteCategory": "Cancella layer",
+        "edit": "Modifica",
+        "delete": "Cancella",
+        "grid": {
+          "name": "Nome",
+          "desc": "Descrizione",
+          "dynType": "Tipo dinamico",
+          "dynValue": "Valore dinamico",
+          "createDate": "Data di creazione",
+          "updateDate": "Data di aggiornamento",
+          "edit": " ",
+          "delete": " "
+        },
+        "notification": {
+          "delete": {
+            "title": "Cancella luogo",
+            "confirm": "Vuoi cancellare questo luogo?",
+            "btnDelete": "Cancella",
+            "btnCancel": "Annulla",
+            "success": "Il luogo e' stato cancellato.",
+            "error": "Questo luogo non puo' essere cancellato; prova piu' tardi.",
+            "cancel": "L'oggetto non puo' essere cancellato."
+          }
+        },
+        "dataTypes": {
+          "nopeusrajoitus": "Limite di velocita'",
+          "vaylatyyppi": "Tipo di bus",
+          "ass_korkeus": "Altezza massima del veicolo",
+          "joukkoliikenteen_pysakki": "Fermata dei mezzi pubblici",
+          "paikannusnimistopiste": "Piertotum locomotor",
+          "liikennevirran_suunta": "Direzione del flusso di traffico",
+          "toiminnallinen_luokka": "Classe funzionale",
+          "tie-elementin_tyyppi": "Tipo di elemento Strada"
+        }
+      },
+      "feedback": {
+          "title": "Feedback",
+          "edit": "Modifica",
+          "publish": "Pubblica",
+          "unpublish": "Annulla la pubblicazione",
+          "delete": "Cancella",
+          "grid": {
+            "name": "Nome",
+            "description": "Descrizione",
+            "createDate": "Data di creazione",
+            "edit": " ",
+            "delete": " "
+          },
+          "notification": {
+            "delete": {
+              "title": "Cancella luogo",
+              "confirm": "Vuoi cancellare questo luogo?",
+              "btnDelete": "Cancella",
+              "btnCancel": "Annulla",
+              "success": "Il luogo e' stato cancellato.",
+              "error": "Questo luogo non puo' essere cancellato; prova piu' tardi.",
+              "cancel": "L'oggeto non puo' essere cancellato."
+            }
+          }
+      },
+      "myviews": {
+        "title": "Le mie viste",
+        "edit": "Modifica",
+        "publish": "Pubblica",
+        "unpublish": "Annulla la pubblicazione",
+        "delete": "Cancella",
+        "grid": {
+          "name": "Nome",
+          "publish": "Data di pubblicazione",
+          "edit": " ",
+          "delete": " "
+        },
+        "popup": {
+          "edit": "Cambia il nome della vista",
+          "title": "Salva la vista",
+          "label": "Nome della vista:",
+          "placeholder": "Dai uno straccio di nome alla vista",
+          "save": "Salva",
+          "cancel": "Annulla",
+          "error_noname": "Digita il nome",
+          "deletetitle": "Conferma la cancellazione",
+          "deletemsg": "Vuoi cancellare questa vista?"
+        },
+        "save": {
+          "success": "La vista e' stata salvata.",
+          "error_noname": "Il campo nome non puo' essere vuoto.",
+          "error_illegalchars": "Il nome contiene caratteri non validi. I caratteri accettati sono le lettere, i numeri, underscore e trattino.",
+          "msg": {
+            "view_name": "Il nome della vista"
+          }
+        },
+        "button": {
+          "ok": "OK",
+          "save": "Salva",
+          "cancel": "Annulla",
+          "toolbarsave": "Salva la vista"
+        },
+        "error": {
+          "title": "Errore!",
+          "generic": "Errore di sistema; riprova piu' tardi.",
+          "notsaved": "La vista non puo' essere salvata.",
+          "notdeleted": "La vista non puo' essere cancellata.",
+          "makePublic": "La vista non puo' essere pubblicata; prova piu' tardi.",
+          "makePrivate": "La pubblicazione non puo' essere annullata; prova piu' tardi.",
+          "loadfailed": "Le viste non possono essere scaricate."
+        }
+      },
+      "publishedmaps": {
+        "title": "Mappe embedded",
+        "edit": "Mostra la mappa",
+        "publish": "Pubblica la mappa",
+        "unpublish": "Annulla la pubblicazione della mappa",
+        "delete": "Cancella",
+        "grid": {
+          "name": "Nome",
+          "domain": "Impostazione della mappa",
+          "publish": "Stato",
+          "edit": " ",
+          "delete": " "
+        },
+        "popup": {
+          "edit": "Cambia il nome della mappa",
+          "title": "Salva la mappa",
+          "label": "Nome della mappa",
+          "placeholder": "Dai un nome alla mappa",
+          "save": "Salva",
+          "cancel": "Annulla",
+          "error_noname": "Dai un nome alla mappa",
+          "deletetitle": "Cancella la mappa?",
+          "deletemsg": "Vuoi cancellare la mappa embedded? Non sara' piu' visibile nel tuo sito."
+        },
+        "save": {
+          "success": "La mappa e' stata salvata.",
+          "error_noname": "Il campo nome non puo' essere vuoto.",
+          "error_illegalchars": "Il nome contiene caratter non validi. I caratteri ammessi sono le lettere, le cifre, underscore e trattino.",
+          "msg": {
+            "view_name": "Nome della mappa"
+          }
+        },
+        "button": {
+          "ok": "OK",
+          "save": "Salva",
+          "cancel": "Annulla",
+          "toolbarsave": "Salva la mappa"
+        },
+        "error": {
+          "title": "Errore!",
+          "generic": "Errore di sistema; riprova piu' tardi.",
+          "notsaved": "La mappa non puo' essere salvata.",
+          "notdeleted": "La mappa non puo' essere salvata.",
+          "makePublic": "La mappa non puo' essere pubblicata; prova piu' tardi.",
+          "makePrivate": "La pubblicazione non puo' essere annullata; riprova piu' tardi."
+        }
+      },
+      "account": {
+        "title": "Il mio account",
+        "firstName": "Nome",
+        "lastName": "Cognome",
+        "nickName": "Nickname",
+        "email": "E-mail",
+        "changeInfo": "Cambia le informazione del tuo account",
+        "changePassword": "Cambia la password",
+        "removeAccount": "Cancella il tuo account"
+      }
+    },
+    "button": {
+      "yes": "Si",
+      "no": "No"
+    }
+  }
+});
