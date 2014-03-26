cd4c7ff4c946a96e2f1d99eecb21cc39a545df23
claudio rocchini <claudio@rockini.name>
2014-03-26 14:10:26 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/promote/locale/it.js
@@ -0,0 +1,8 @@
+Oskari.registerLocalization({
+  "lang": "it",
+  "key": "Promote",
+  "value": {
+    "title": "Utenti registrati",
+    "desc": "Gli utenti registrati hanno accesso a funzionalita' aggiuntive."
+  }
+});
