e3447e7fc73e09e0db92a260b955cf039dbd3de3
claudio rocchini <claudio@rockini.name>
2014-03-26 14:01:42 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/mapmyplaces/locale/it.js
@@ -0,0 +1,13 @@
+Oskari.registerLocalization({
+    "lang" : "it",
+    "key" : "MapMyPlaces",
+    "value" : {
+        "title" : "I miei posti",
+        "desc" : "",
+        "object-data": "Dati degli oggetti",
+        "layer" : {
+            "organization" : "I miei posti",
+            "inspire" : "I miei layer"
+        }
+    }
+});
