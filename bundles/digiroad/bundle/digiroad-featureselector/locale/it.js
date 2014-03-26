2ee08a94ae693801e9f1806f7e14134793ff7199
claudio rocchini <claudio@rockini.name>
2014-03-26 14:30:23 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/digiroad/bundle/digiroad-featureselector/locale/it.js
@@ -0,0 +1,27 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "FeatureSelector",
+	"value" : {
+		"title" : "Seleziona Feature",
+		"desc" : "",
+        "success" : "Dati spediti correttamente. Puoi vedere le feature modificate in 'Miei Dati -> Feature Modificate'",
+		"errors" : {
+            "title": "Errore!",
+            "generic": "Errore di sistema: riprova.",
+			"loadFailed" : "Errore di caricamento delle feature. Aggiorna la pagina del browser.",
+            "noResults": "Nessun risultato.",
+            "dataSendFailed": "Errore durante la spedizione dei dati. Questo errore e' stato registrato."
+		},
+		"notification": {
+            "featureEdited": {
+                "title": "Dati spediti correttamente",
+                "message": "Puoi vedere le feature modificate in 'Miei Dati'."
+            },
+            "uneditable": {
+                "title": "Nessuna modifica",
+                "message": "Impossibile stabilizzare i ciclotroni quantici."
+            }
+        },
+        "loading" : "Caricamento..."
+	}
+});
