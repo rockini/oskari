5f76344c94fefbefa56c947f4f5e874bd87ffc90
claudio rocchini <claudio@rockini.name>
2014-03-26 13:56:26 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/layerselection2/locale/it.js
@@ -0,0 +1,52 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "LayerSelection",
+    "value": {
+        "title": "Layer selezionati",
+        "desc": "",
+        "layer": {
+            "style": "Stile",
+            "show": "Mostra",
+            "hide": "Nascondi",
+            "hidden": "La mappa e' stata (temporanemente) nascosta.",
+            "out-of-scale": "I dati della mappa non possono essere visualizzati a questa scala.",
+            "move-to-scale": "Vai ad un livello di scala decente.",
+            "out-of-content-area": "Il layer non contiene dati in questa posizione.",
+            "move-to-content-area": "Localizza",
+            "description": "Descrizione",
+            "object-data": "Dati degli oggetti",
+            "rights": {
+                "notavailable": "La pubblicazione e' proibita.",
+                "guest": "Fai login per pubblicare questi dati.",
+                "loggedin": "La pubblicazione e' permessa",
+                "official": "La pubblicazione e' autorizzata dai responsabili.",
+                "need-login": "Devi fare login.",
+                "can_be_published_by_provider": {
+                    "label": "La pubblicazione e' autorizzata dai fornitori dei dati.",
+                    "tooltip": "L'autorizzazione dei fornitori è richiesta per pubblicare questo layer. Se sei il fornitore dei dati, contatta il supporto di Paikkatietoikkuna e richiedi il permesso per la pubblicazione."
+                },
+                "can_be_published": {
+                    "label": "Pubblicazione permessa",
+                    "tooltip": "Il layer puo' essere pubblicato in una mappa embedded senza limitazioni di numero di utenti."
+                },
+                "can_be_published_map_user": {
+                    "label": "Pubblicazione permessa",
+                    "tooltip": "Il layer puo' essere pubblicata in una mappa embedded ma il numero di utenti per settimana e' limitato."
+                },
+                "no_publication_permission": {
+                    "label": "Pubblicazione proibita.",
+                    "tooltip": "Il fornitore dei dati non ha rilasciato il permesso di pubblicazione di questi dati."
+                },
+                "can_be_published_by_authority": {
+                    "label": "Pubblicazione permessa",
+                    "tooltip": "Il layer puo' essere pubblicato in una mappa embedde senza limitazioni d'uso."
+                }
+            },
+            "tooltip": {
+                "type-base": "Mappa di background",
+                "type-wms": "Layer",
+                "type-wfs": "Data product"
+            }
+        }
+    }
+});
