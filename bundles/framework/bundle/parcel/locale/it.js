5f6d043d9c2af4d03a0bf8aec10c34d3c510ac7e
claudio rocchini <claudio@rockini.name>
2014-03-26 14:06:40 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/parcel/locale/it.js
@@ -0,0 +1,101 @@
+Oskari.registerLocalization({
+  "lang": "it",
+  "key": "Parcel",
+  "value": {
+    "title": "Particelle",
+    "desc": "",
+    "tools": {
+      "line": {
+        "tooltip": "Disegna una linea",
+        "new": "Dividi la linea di destinazione. Aggiungi un nodo cliccando sulla mappa. Per interrompere il disegno fai doppio click.",
+        "edit": "Modifica la linea trascinandone i nodi.",
+        "save": "Salva"
+      },
+      "area": {
+        "tooltip": "Disegna un'area",
+        "new": "Disegna l'area. Aggiungi i nodi cliccando sulla mappa. Per interrompere fai doppio click.",
+        "edit": "Per modificare l'area trascina i nodi.",
+        "save": "Salva"
+      },
+      "selector": {
+          "tooltip": "Modifica la selezione di una colonna"
+      },
+      "clear": {
+          "tooltip": "Annulla"
+      },
+      "save": {
+          "tooltip": "Fai un disegno e salvalo"
+      },
+      "debug": {
+          "tooltip": "Interrompi il disegno"
+      },
+      "tooltip": "Cerca la prima proprieta'"
+    },
+    "buttons": {
+      "ok": "OK",
+      "cancel": "Annulla",
+      "print": "Stampa",
+      "save": "Salva",
+      "movePlaces": "Sposta i luoghi"
+    },
+      "placeform": {
+          "title": "Informazioni sulle particelle",
+          "tooltip": "Immettere le informazioni sulla particella da stampare o salvare",
+          "placename": {
+              "placeholder": "Icona"
+          },
+          "ptitle": {
+              "placeholder": "Titolo del disegno"
+          },
+          "subtitle": {
+              "placeholder": "Sotto-disegno"
+          },
+          "description": {
+              "placeholder": "Panoramica"
+          },
+          "parent_property_id": {
+              "placeholder": "ID Archivio di stato"
+          },
+          "parent_property_quality": {
+              "placeholder": "Dati di origine Archivio di stato"
+          },
+          "reporter": {
+              "placeholder": "Digita l'autore"
+          },
+          "area": {
+              "placeholder": "Area (m²)"
+          }
+      },
+      "qualitycodes": {
+          "q0": "(Sconosciuto)",
+          "q4": "Misura sull'immagine",
+          "q14": "Digitalizzazione",
+          "q20": "Misura sul terreno",
+          "q30": "Termini immobiliari",
+          "q50": "Formula di calcolo"
+      },
+    "notification": {
+      "placeLoading": {
+        "title": "Elementi caricati",
+        "message": ""
+      },
+      "calculating": {
+        "title": "Calcolo della distribuzione",
+        "message": ""
+      },
+      "placeAdding": {
+        "title": "Elementi memorizzati",
+        "message": ""
+      },
+      "error": {
+        "title": "Errore!",
+        "savePlace": "Errore di memoria. ID oggetto già in uso.",
+        "loadPlace": "L'oggetto non può essere caricato"
+      }
+    },
+    "validation": {
+      "title": "Errore:",
+      "placeName": "Nome dell'oggetto mancante."
+    }
+  }
+});
