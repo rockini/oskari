a0f6625310f3f0ac2d4506d5de3bbc90219f7381
claudio rocchini <claudio@rockini.name>
2014-03-26 14:14:34 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/search/locale/it.js
@@ -0,0 +1,31 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "Search",
+    "value": {
+        "title": "Cerca Luoghi",
+        "desc": "",
+        "contentErrorMsg": "La stringa di ricerca contiene caratteri non validi.",
+        "searchDescription": "Cerca per nome, indirizzo, o codice di particella catastale.",
+        "searchButton": "Cerca",
+        "searchResultCount": "La ricerca ha ottenuto ",
+        "searchResultCount2": " risulati.",
+        "searchResultDescriptionMoreResults": "Raffina la ricerca per diminuire il numero di risultati.",
+        "searchResultDescriptionOrdering": "Puoi ordinare i risultati della seguente tabella cliccando sull'intestazione di una colonna.",
+        "searchResults": "Risultati: ",
+        "searchResultsDescription": " risultati ",
+        "searchservice_search_alert_title": "Errore: ",
+        "searchservice_search_not_found_anything_text": "La ricerca non ha ottenuto risultati.",
+        "too_short": "La stringa di ricerca e' troppo corta.",
+        "cannot_be_empty": "La stringa di ricerca non puo' essere vuota",
+        "too_many_stars": "Troppi asterischi (*)",
+        "grid": {
+            "name": "Nome",
+            "village": "Comune",
+            "type": "Tipo"
+        },
+        "resultBox": {
+            "close": "Chiudi",
+            "title": "Risultati della ricerca"
+        }
+    }
+});
