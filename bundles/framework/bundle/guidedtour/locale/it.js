10c867eaf6d7fa10cfa0ee298955fc8c706274a5
claudio rocchini <claudio@rockini.name>
2014-03-26 13:55:28 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/guidedtour/locale/it.js
@@ -0,0 +1,69 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "GuidedTour",
+    "value": {
+        "button": {
+            "next": "Succesivo",
+            "previous": "Precedente",
+            "start": "Inizio",
+            "finish": "Fine",
+            "close": "Chiudi"
+        },
+        "title": "Buongiorno",
+        "desc": "",
+        "tourseen": {
+            "label": "Non mostrare piu' questo messaggio"
+        },
+        "page1": {
+            "title": "Benvenuto a Paikkatietoikkuna",
+            "message": "Se e' la prima volta che visiti Paikkatietoikkuna, puoi seguire la nostra visita guidata."
+        },
+        "page2": {
+            "title": "Ricerca",
+            "message": "Usa la funzione di ricerca per muovere la mappa direttamente sul luogo che ti interessa",
+            "openLink": "Mostra ricerca",
+            "closeLink": "Nascondi ricerca",
+            "tileText": "Ricerca"
+        },
+        "page3": {
+            "title": "Layer",
+            "message": "Puoi selezionare le cose da vedere nella mappa attraverso i layer. Clicca su una catergoria e apri il layer corrispondente, oppure digita le lettere iniziali del nome nel campo di ricerca.",
+            "openLink": "Mostra layer",
+            "closeLink": "Nascondi layer",
+            "tileText": "Layer"
+        },
+        "page4": {
+            "title": "Layer selezionati",
+            "message": "Puoi cambiare l'ordine di visualizzazione dei layer, cliccandoci e trascinandoli, in modo da cambiarne la trasparenza. Le modifiche saranno visualizzate sulla mappa.",
+            "openLink": "Mostra layer selezionati",
+            "closeLink": "Nascondi layer selezionati",
+            "tileText": "Layer selezionati"
+        },
+        "page5": {
+            "title": "Miei dati",
+            "message": "Puoi salvare nel tuo account, tutti i luoghi che hai selezionato, cosi' come tutte le viste e le mappe embedded.",
+            "openLink": "Mostra miei dati",
+            "closeLink": "Nascondi miei dati",
+            "tileText": "Miei dati"
+        },
+        "page6": {
+            "title": "Crea una mappa",
+            "message": "Puoi inserire facilmente una mappa che hai creato nel tuo sito. Basta definirne la dimensione e l'interfaccia utente, quindi copiare ed incollare il codice HTML prodotto.",
+            "openLink": "Mostra creazione mappa",
+            "closeLink": "Nascondi creazione mappa",
+            "tileText": "Crea mappa"
+        },
+        "page7": {
+            "title": "Strumenti",
+            "message": "Utilizza gli strumenti per aggiungere luoghi, salvare mappe e viste, condividere link alle viste. Posizione il puntatore del mouse su uno strumento per avere un breve aiuto sullo stesso."
+        },
+        "page8": {
+            "title": "Muoviti sulla mppa",
+            "message": "Puoi spostarti sulla mappa col mouse; oppure tornare alla vista di default cliccando sul bottone in mezzo ai controlli di movimento. Suggerimento: puoi spostarti anche con la tastiera."
+        },
+        "page9": {
+            "title": "Zummami!",
+            "message": "Puoi zummare avanti ed indietro usando la scrollbar della scala oppure i pulsanti piu' e meno. Suggerimento: puoi usare anche il piu' e meno della tastiera o fare doppio click sulla mappa. Infine puoi zummare con la rotellina del mouse."
+        }
+    }
+});
