b1d5be1388f5833c1b2f809e2dc68cbb0cb42b2d
claudio rocchini <claudio@rockini.name>
2014-03-26 14:00:45 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/mapmodule-plugin/locale/it.js
@@ -0,0 +1,74 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "MapModule",
+    "value": {
+        "status_update_map": "Aggiornamento mappa",
+        "zoombar_tooltip": {
+            "zoomLvl-0": "Mappa di background",
+            "zoomLvl-1": "Stato",
+            "zoomLvl-2": "Regione",
+            "zoomLvl-3": "Provincia",
+            "zoomLvl-4": "Citta'",
+            "zoomLvl-5": "",
+            "zoomLvl-6": "",
+            "zoomLvl-7": "Parte di citta'",
+            "zoomLvl-8": "",
+            "zoomLvl-9": "",
+            "zoomLvl-10": "via",
+            "zoomLvl-11": "",
+            "zoomLvl-12": "Brucoli sui marciapiedi"
+        },
+        "plugin": {
+            "LogoPlugin": {
+                "terms": "Condizioni di utilizzo",
+                "dataSources": "Sorgenti dei dati",
+                "layersHeader": "Layers",
+                "indicatorsHeader": "Indicatori"
+            },
+            "DataSourcePlugin": {
+                "link": "Sorgenti dei dati",
+                "popup": {
+                    "title": "Sorgenti dei dati",
+                    "content": " "
+                },
+                "button": {
+                    "close": "Chiudi"
+                }
+            },
+            "LayerSelectionPlugin": {
+                "title": "Dati degli oggetti",
+                "chooseDefaultBaseLayer": "Seleziona il layer di background"
+            },
+            "SearchPlugin": {
+                "placeholder": "Cerca un posto",
+                "search": "Cerca",
+                "close": "Chiudi i risultati della ricerca",
+                "noresults": "La ricerca non ha prodotto risultati",
+                "toomanyresults": "Piu' di cento risultati; si prega di specificare meglio la ricerca.",
+                "column_name": "Nome",
+                "column_village": "Coumne",
+                "column_type": "Tipo"
+            },
+            "GetInfoPlugin": {
+                "title": "Layer",
+                "layer": "Layer",
+                "places": "Posti",
+                "description": "Descrizione",
+                "link": "URL",
+                "name": "Nome"
+            },
+            "PublisherToolbarPlugin": {
+                "test": "Test",
+                "history": {
+                    "reset": "Ritorna la vista di default",
+                    "back": "indientro",
+                    "next": "avanti"
+                },
+                "measure": {
+                    "line": "Misura distanza",
+                    "area": "Misura area"
+                }
+            }
+        }
+    }
+});
