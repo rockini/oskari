a364b115e52ed01265baf9d0ad1e660f4338693d
claudio rocchini <claudio@rockini.name>
2014-03-26 13:52:21 +0100

    Create it.js
    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/divmanazer/locale/it.js
@@ -0,0 +1,99 @@
+Oskari.registerLocalization({
+  "lang": "it",
+  "key": "DivManazer",
+  "value": {
+    "VisualizationForm": {
+      "tooltips": {
+        "dot": "Modifica lo stile dei punti",
+        "area": "Modifica lo stile delle aree",
+        "line": "Modifica lo stile delle linee"
+      },
+      "dot": {
+        "title": "Rappresentazione dei punti",
+        "tooltip": "",
+        "symbol": {
+          "label": "Icone dei punti"
+        },
+        "size": {
+          "label": "Dimensione"
+        },
+        "color": {
+          "label": "Colore",
+          "labelOr": "o",
+          "labelCustom": "Colore RGB personalizzato (0-255)"
+        },
+        "preview": {
+          "label": "Preview"
+        },
+        "buttons": {
+          "save": "Salva",
+          "cancel": "Cancella"
+        }
+      },
+      "line": {
+        "title": "Rappresentazione delle linee",
+        "tooltip": "",
+        "style": {
+          "label": "Stile della linea"
+        },
+        "cap": {
+          "label": "Estremi della linea"
+        },
+        "corner": {
+          "label": "Angoli"
+        },
+        "width": {
+          "label": "Spessore"
+        },
+        "color": {
+          "label": "Colore",
+          "labelOr": "o",
+          "labelCustom": "Colore RGB personalizzato (0-255)"
+        },
+        "preview": {
+          "label": "Preview"
+        },
+        "buttons": {
+          "save": "Salva",
+          "cancel": "Cancella"
+        }
+      },
+      "area": {
+        "title": "Rappresentazione delle aree",
+        "tooltip": "",
+        "linestyle": {
+          "label": "Stile del bordo"
+        },
+        "linecap": {
+          "label": "Estremi del bordo"
+        },
+        "linecorner": {
+          "label": "Angoli"
+        },
+        "linewidth": {
+          "label": "Spessore"
+        },
+        "linecolor": {
+          "label": "Colore del bordo",
+          "labelOr": "o",
+          "labelCustom": "Colore RGB personalizzato (0-255)"
+        },
+        "color": {
+          "label": "Colore del riempimento",
+          "labelOr": "o",
+          "labelCustom": "Colore RGB personalizzato (0-255)"
+        },
+        "fill": {
+          "label": "Pattern di riempimento"
+        },
+        "preview": {
+          "label": "Preview"
+        },
+        "buttons": {
+          "save": "Salva",
+          "cancel": "Annulla"
+        }
+      }
+    }
+  }
+});
