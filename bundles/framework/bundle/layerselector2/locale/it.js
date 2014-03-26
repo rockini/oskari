16169d01112ac353a45eed13370d1d54d67f61f5
claudio rocchini <claudio@rockini.name>
2014-03-26 13:57:15 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/layerselector2/locale/it.js
@@ -0,0 +1,64 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "LayerSelector",
+    "value": {
+        "title": "Layer",
+        "desc": "",
+        "errors": {
+            "title": "Errore!",
+            "generic": "Errore di sistema; riprova piu' tardi.",
+            "loadFailed": "Errore nel caricamento del layer. Ricarca la pagina nel browser e seleziona il layer.",
+            "noResults": "La ricerca non ha prodotto risultati.",
+            "noResultsForKeyword": "La ricerca non ha trovato nessuna keyword.",
+            "minChars": "Minimo 4 caratteri."
+        },
+        "loading": "Searching...",
+        "filter": {
+            "text": "Cerca layer",
+            "keywordsTitle": "Keyword:",
+            "shortDescription": "Cerca un layer per nome, produttore o keyword.",
+            "description": "Puoi cercare i layer scrivendo una parte del nome, del produttore, o le keyword associate. La ricerca per keyword parte dopo il quarto carattere.",
+            "inspire": "Per tema",
+            "organization": "Per fornitore",
+            "published": "Utenti",
+            "didYouMean": "Forse intendevi dire:"
+        },
+        "published": {
+            "organization": "Layer pubblicati",
+            "inspire": "Layer pubblicati"
+        },
+        "tooltip": {
+            "type-base": "Mappa di background",
+            "type-wms": "Layer",
+            "type-wfs": "Data product"
+        },
+        "backendStatus": {
+            "OK": {
+                "tooltip": "Il layer e' disponibile.",
+                "iconClass": "backendstatus-ok"
+            },
+            "DOWN": {
+                "tooltip": "Il layer non e' disponibile.",
+                "iconClass": "backendstatus-down"
+            },
+            "MAINTENANCE": {
+                "tooltip": "Il layer potrebbe non essere disponibile nei prossimi giorni.",
+                "iconClass": "backendstatus-maintenance"
+            },
+            "UNKNOWN": {
+                "tooltip": "Boh",
+                "iconClass": "backendstatus-ok"
+            }
+        },
+        "buttons": {
+            "ok": "OK"
+        },
+        "types": {
+            "syn": "Sinonimo",
+            "lk": "Concetto correlato",
+            "vk": "Concetto coordinato",
+            "ak": "Concetto subordinato",
+            "yk": "Concetto sovraordinato"
+        }
+    }
+});
