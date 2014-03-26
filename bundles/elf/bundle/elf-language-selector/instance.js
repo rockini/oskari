85181d5532129844ae5b663b42d78667d24aa3d8
claudio rocchini <claudio@rockini.name>
2014-03-26 14:54:57 +0100

    Italian support added

--- bundles/elf/bundle/elf-language-selector/instance.js
+++ bundles/elf/bundle/elf-language-selector/instance.js
@@ -6,24 +6,26 @@ Oskari.clazz.define("Oskari.elf.languageselector.BundleInstance", function() {
 }, {
     templates : {
         "fi" : {
-            "content" : '<a href="?lang=fi#">suomi</a><br /><br /><a href="?lang=sv#">ruotsi</a><br />' + '<a href="?lang=en#">englanti</a><br /><br /><a href="?lang=de#">saksa</a><br />' + '<a href="?lang=es#">espanja</a><br /><br /><a href="?lang=cs#">tsekki</a><br />'
+            "content" : '<a href="?lang=fi#">suomi</a><br /><br /><a href="?lang=sv#">ruotsi</a><br />' + '<a href="?lang=en#">englanti</a><br /><br /><a href="?lang=de#">saksa</a><br />' + '<a href="?lang=es#">espanja</a><br /><br /><a href="?lang=cs#">tsekki</a><br />' + '<a href="?lang=it#">italialainen</a><br />'
         },
         "sv" : {
-            "content" : '<a href="?lang=fi#">finska</a><br /><br /><a href="?lang=sv#">svenska</a><br />' + '<a href="?lang=en#">engelska</a><br /><br /><a href="?lang=de#">tyska</a><br />' + '<a href="?lang=es#">spanska</a><br /><br /><a href="?lang=cs#">tjeckiska</a><br />'
+            "content" : '<a href="?lang=fi#">finska</a><br /><br /><a href="?lang=sv#">svenska</a><br />' + '<a href="?lang=en#">engelska</a><br /><br /><a href="?lang=de#">tyska</a><br />' + '<a href="?lang=es#">spanska</a><br /><br /><a href="?lang=cs#">tjeckiska</a><br />' + '<a href="?lang=it#">Italienska</a><br />'
         },
         "de" : {
-            "content" : '<a href="?lang=fi#">Finnisch</a><br /><br /><a href="?lang=sv#">Schwedisch</a><br />' + '<a href="?lang=en#">Englisch</a><br /><br /><a href="?lang=de#">Deutsch</a><br />' + '<a href="?lang=es#">Spanisch</a><br /><br /><a href="?lang=cs#">Tschechisch</a><br />'
+            "content" : '<a href="?lang=fi#">Finnisch</a><br /><br /><a href="?lang=sv#">Schwedisch</a><br />' + '<a href="?lang=en#">Englisch</a><br /><br /><a href="?lang=de#">Deutsch</a><br />' + '<a href="?lang=es#">Spanisch</a><br /><br /><a href="?lang=cs#">Tschechisch</a><br />' + '<a href="?lang=it#">Italienisch</a><br />'
         },
         "en" : {
-            "content" : '<a href="?lang=fi#">Finnish</a><br /><br /><a href="?lang=sv#">Swedish</a><br />' + '<a href="?lang=en#">English</a><br /><br /><a href="?lang=de#">German</a><br />' + '<a href="?lang=es#">Spanish</a><br /><br /><a href="?lang=cs#">Czech</a><br />'
+            "content" : '<a href="?lang=fi#">Finnish</a><br /><br /><a href="?lang=sv#">Swedish</a><br />' + '<a href="?lang=en#">English</a><br /><br /><a href="?lang=de#">German</a><br />' + '<a href="?lang=es#">Spanish</a><br /><br /><a href="?lang=cs#">Czech</a><br />' + '<a href="?lang=it#">Italian</a><br />'
         },
         "cs" : {
-            "content" : '<a href="?lang=fi#">Finština</a><br /><br /><a href="?lang=sv#">Švédština</a><br />' + '<a href="?lang=en#">Angličtina</a><br /><br /><a href="?lang=de#">Němčina</a><br />' + '<a href="?lang=es#">Španělština</a><br /><br /><a href="?lang=cs#">Čeština</a><br />'
+            "content" : '<a href="?lang=fi#">Finština</a><br /><br /><a href="?lang=sv#">Švédština</a><br />' + '<a href="?lang=en#">Angličtina</a><br /><br /><a href="?lang=de#">Němčina</a><br />' + '<a href="?lang=es#">Španělština</a><br /><br /><a href="?lang=cs#">Čeština</a><br />'  + '<a href="?lang=it#">italština</a><br />'
         },
         "es" : {
-            "content" : '<a href="?lang=fi#">finés</a><br /><br /><a href="?lang=sv#">sueco</a><br />' + '<a href="?lang=en#">inglés</a><br /><br /><a href="?lang=de#">alemán</a><br />' + '<a href="?lang=es#">español</a><br /><br /><a href="?lang=cs#">checo</a><br />'
+            "content" : '<a href="?lang=fi#">finés</a><br /><br /><a href="?lang=sv#">sueco</a><br />' + '<a href="?lang=en#">inglés</a><br /><br /><a href="?lang=de#">alemán</a><br />' + '<a href="?lang=es#">español</a><br /><br /><a href="?lang=cs#">checo</a><br />'  + '<a href="?lang=it#">italiano</a><br />'
+        },
+		"it" : {
+            "content" : '<a href="?lang=fi#">Finlandese</a><br /><br /><a href="?lang=sv#">Svedese</a><br />' + '<a href="?lang=en#">Inglese</a><br /><br /><a href="?lang=de#">Tedesco</a><br />' + '<a href="?lang=es#">Spagnolo</a><br /><br /><a href="?lang=cs#">Ceco</a><br />' + '<a href="?lang=it#">Italiano</a><br />'
         }
-
     },
 
     locales : {
@@ -74,6 +76,14 @@ Oskari.clazz.define("Oskari.elf.languageselector.BundleInstance", function() {
             },
             "popover" : {
                 "title" : "Jazyk"
+            }
+        },
+		"it" : {
+            "tile" : {
+                "title" : "Lingua"
+            },
+            "popover" : {
+                "title" : "Cambia lingua"
             }
         }
 
