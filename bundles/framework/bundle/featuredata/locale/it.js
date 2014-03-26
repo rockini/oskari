a4aafb49fa57f0b465a35d283d2c5e266b13cb80
claudio rocchini <claudio@rockini.name>
2014-03-26 13:53:19 +0100

    Create it.js
    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/featuredata/locale/it.js
@@ -0,0 +1,48 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "FeatureData",
+    "value": {
+        "title": "Dati degli oggetti",
+        "desc": "",
+        "loading": "Caricamento",
+        "showmore": "Mostr",
+        "nodata": "La mappa selezionata non contiene dati di attributi.",
+        "featureNameAll": "Tipo di dati",
+        "errorscale": "I dati non possono essere mostrati a questa scala; cambia scala.",
+        "errordata": "I dati del server contengono errori. Muovi leggermente la mappa per aggiornarli.",
+        "columnSelectorTooltip": "Mostra Colonne",
+        "popup": {
+            "title": "Seleziona le feature nella mappa",
+            "instructions": " ",
+            "link": {
+                "title": "Seleziona piu' cose"
+            },
+            "button": {
+                "cancel": "Annulla",
+                "show": "Mostra luoghi",
+                "close": "Chiudi",
+                "edit": "Modifica"
+            },
+            "tools": {
+                "point": {
+                    "tooltip": "Aggiungi un punto"
+                },
+                "line": {
+                    "tooltip": "Aggiungi una linea"
+                },
+                "polygon": {
+                    "tooltip": "Aggiungi un poligono"
+                },
+                "square": {
+                    "tooltip": "Aggiungi un rettangolo"
+                },
+                "circle": {
+                    "tooltip": "Aggiungi un cerchio"
+                },
+                "select": {
+                    "tooltip": "Seleziona gli oggetti"
+                }
+            }
+        }
+    }
+});
