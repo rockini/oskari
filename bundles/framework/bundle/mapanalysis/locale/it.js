607856257fc4f827bad70aff08e2618c9e96bfd2
claudio rocchini <claudio@rockini.name>
2014-03-26 13:58:06 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/mapanalysis/locale/it.js
@@ -0,0 +1,13 @@
+Oskari.registerLocalization({
+    "lang" : "it",
+    "key" : "MapAnalysis",
+    "value" : {
+        "title" : "Analisi",
+        "desc" : "",
+        "object-data": "Dati degli oggetti",
+         "layer" : {
+                "organization" : "Analisi dell'utente",
+                "inspire" : "Analisi dell'utente"
+            }
+    }
+});
