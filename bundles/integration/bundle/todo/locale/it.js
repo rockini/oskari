350c06fdf2edce4560d375d1549abc435f672a2d
claudio rocchini <claudio@rockini.name>
2014-03-26 14:21:57 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/integration/bundle/todo/locale/it.js
@@ -0,0 +1,23 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "todo",
+	"value" : {
+		"title" : "Vista ToDo",
+		"desc" : "",
+		"flyout" : {
+			"title" : "Vista ToDo!"
+		},
+		"tile" : {
+			"title" : "ToDo",
+			"tooltip" : "."
+		},
+		"view" : {
+			"title" : "",
+			"prompt" : "",
+			"templates" : {
+			}
+
+		}
+
+	}
+});
