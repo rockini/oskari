e7aa1174a09958b6ae1aba32e400ad94af4d0f3e
claudio rocchini <claudio@rockini.name>
2014-03-26 14:18:44 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/harava/bundle/harava-draw/locale/it.js
@@ -0,0 +1,19 @@
+Oskari.registerLocalization({
+	"lang" : "it",
+	"key" : "HaravaDraw",
+	"value" : {
+		"title" : "",
+		"desc" : "",
+		"display" : {
+			"tooltips" : {
+				"drawPoint": "Aggiungi una geometria puntuale",
+				"drawLine": "Aggiungi una geometria lineare",
+				"drawPolygon": "Aggiungi una geometria areale",
+				"deleteSelectedDraw": "Cancella le geometrie selezionate",
+				"selectGeometry" : "Seleziona una geometria"
+			},
+			"notSelected" : "Nessuna geometria selezionata",
+			"confirmDelete" : "Cancella le geometrie selezionate?"
+		}
+	}
+});
