32d02a2ee64dd479f24965adf68a0c1a087afaa1
claudio rocchini <claudio@rockini.name>
2014-03-26 14:36:27 +0100

    it translation from Istituto Geografico Militare?

---
+++ src/framework/guidedtour/locale/it.js
@@ -0,0 +1,71 @@
+Oskari.registerLocalization(
+    {
+        "lang": "it",
+        "key": "GuidedTour",
+        "value": {
+            "button": {
+                "next" : "Prossimo",
+                "previous" : "Precedente",
+                "start" : "Inizio",
+                "finish" : "Fine",
+                "close" : "Chiudi"
+            },
+            "title": "Salve",
+            "desc": "",
+            "tourseen" : {
+                "label" : "Non mostrare piu' questo messaggio"
+            },
+            "page1": {
+                "title": "Benvenuto in Paikkatietoikkuna",
+                "message" : "Se e' la prima volta che visiti Paikkatietoikkuna, la nostra visita guidata ti potra' introdurre all'utilizzo."
+            },
+            "page2": {
+                "title": "Cerca",
+                "message" : "Usa la funzione di ricerca per trovare il posto che ti interessa.",
+                "openLink" : "Mostra cerca",
+                "closeLink" : "Nascondi cerca",
+                "tileText" : "Cerca"
+            },
+            "page3": {
+                "title": "Layer",
+                "message" : "Puoi scegliere i dati da visulizzare sulla mappa, selezionando gli opportuni layer. Clicca sulla categoria di interesse e seleziona un layer digitando le prime lettere del nome nel campo di ricerca.",
+                "openLink" : "Mostra layer",
+                "closeLink" : "Nascondi layer",
+                "tileText" : "Layer"
+            },
+            "page4": {
+                "title": "Seleziona i layer",
+                "message" : "Puoi cambiare l'ordine con cui i layer sono disegnati sulla mappa, cosi' come puoi cambiare il loro livello di trasparenza. Le modifiche saranno visualizzate sulla mappa.",
+                "openLink" : "Mostra i layer selezionati",
+                "closeLink" : "Nascondi i layer selezionati",
+                "tileText" : "Layer selezionati"
+            },
+            "page5": {
+                "title": "Miei Dati",
+                "message" : "Tutti i luoghi che ai marcato sulla mappa, le viste e le mappe embedded saranno visualizzate nella voce 'Miei dati' dopo aver fatto login.",
+                "openLink" : "Mostra Miei Dati",
+                "closeLink" : "Nascondi Miei Dati",
+                "tileText" : "Miei Dati"
+            },
+            "page6": {
+                "title": "Crea mappa",
+                "message" : "Puoi facilmente inserire una mappa da te creata nel tuo sito web. Basta definirne la dimensione e l'interfaccia utente, quindi incollare il codice HTML nel tuo sito",
+                "openLink" : "Mostra crea mappa",
+                "closeLink" : "Nascondi crea mappa",
+                "tileText" : "Crea mappa"
+            },
+            "page7": {
+                "title": "Strumenti",
+                "message" : "Usa gli strumenti per aggiungere luoghi, salvare viste, condividere i link alle viste. Lascia fermo il puntatore del mouse su di uno strumenti per avere un suggerimento sullo stesso."
+            },
+            "page8": {
+                "title": "Spostarsi sulla mappa",
+                "message" : "Puoi spostare la vista della mappa trascinando il mouse o utilizzando gli strumenti di movimento. Puoi tornare alla vista di default cliccando sul pulsante centrale in mezzo ai controlli di movimento. Suggerimento: e' possibile spostarsi anche con le frecce della tastiera."
+            },
+            "page9": {
+                "title": "Zoom avanti ed indietro",
+                "message" : "Puoi zummare avandi ed indietro utilizzando la scrolbar della scala o i bottoni piu' e meno. Suggerimento: puo' zummare anche con i tasti piu' e meno della tastiere o facendo doppio click sulla mappa. Infine, e' possibile zummare anche col la rotella del mouse."
+            }
+        }
+    }
+);
