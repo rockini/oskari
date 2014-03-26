13c0fae87b616ac760fe2dbea749d3d52e500ca1
claudio rocchini <claudio@rockini.name>
2014-03-26 14:23:54 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/sample/bundle/helloworld/locale/it.js
@@ -0,0 +1,14 @@
+Oskari.registerLocalization({
+    "lang" : "it",
+    "key" : "helloworld",
+    "value" : {
+        "title" : "Ciao mamma",        
+        "tile" : {
+            "title" : "Ciao mappa!"
+        },
+        "flyout" : {
+            "title" : "Ciao mappa!",
+            "message" : "!!!Ciao mappa!!!!"
+        }
+    }
+});
