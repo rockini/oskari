30f303390f118a32dcd7c3b3724accf47191e7e5
claudio rocchini <claudio@rockini.name>
2014-03-26 14:03:57 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/mapwfs2/locale/it.js
@@ -0,0 +1,40 @@
+Oskari.registerLocalization({
+    "lang" : "it",
+    "key" : "MapWfs2",
+    "value" : {
+        "title" : "MapWfs2",
+        "desc" : "",
+        "object-data": "Dati degli oggetti",
+        "default-style": "Stile di default",
+        "own-style": "Stile personalizzato",
+        "tile" : {
+            "title" : "MapWfs2"
+        },
+        "flyout" : {
+            "title" : "MapWfs2",
+            "message" : "MapWfs2"
+        },
+        "error" : {
+            "title": "Errore",
+            "connection_not_available": "Connessione al WFS non disponibile.",
+            "connection_broken": "Connessione al WFS stroncata.",
+            "wfs_no_permissions": "Permesso negato per l'utilizzo del layer WFS ({layer}).",
+            "wfs_configuring_layer_failed": "Configurazione del layer WFS ({layer}) fallita.",
+            "wfs_request_failed": "Richiesta del WFS layer ({layer}) fallita.",
+            "features_parsing_failed": "Fallito il parsin del WFS layer ({layer})."
+        },
+        "button": {
+            "cancel": "Annulla",
+            "show": "Visualizza luoghi",
+            "close": "Chiudi",
+            "edit" : "Modifica"
+        },
+        "popup": {
+            "title": "Stile personalizzato",
+            "button": {
+                "cancel": "Annulla",
+                "save": "Salva"
+            }
+        }
+    }
+});
