09284e42bb92a775f97fbe464b73d976fe53036c
claudio rocchini <claudio@rockini.name>
2014-03-26 14:22:46 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/lupapiste/bundle/lupapiste-myplaces2/locale/it.js
@@ -0,0 +1,142 @@
+Oskari.registerLocalization({
+  "lang": "it",
+  "key": "MyPlaces2",
+  "value": {
+    "title": "Luoghi",
+    "desc": "",
+    "category": {
+      "defaultName": "Miei layer",
+      "organization": "Miei luoghi",
+      "inspire": "Luoghi"
+    },
+    "guest": {
+      "loginShort": "Fai Login"
+    },
+    "tools": {
+      "point": {
+        "tooltip": "Aggiungi punto",
+        "new": "Aggiungi un punto cliccando sulla mappa.",
+        "edit": "Sposta i punti trascinandoli.",
+        "save": "Salva punto"
+      },
+      "line": {
+        "tooltip": "Aggiungi linea",
+        "new": "Aggiungi i nodi cliccando sulla mappa. Ferma il disegno facendo doppio-click o selezionando 'Ferma disegno'.",
+        "edit": "Modifica la linea trascinandone i nodi.",
+        "save": "Salva la linea"
+      },
+      "area": {
+        "tooltip": "Aggiungi area",
+        "new": "Aggiungi nodi cliccando sulla mappa. Ferma il disegno facendo doppio-click o selezionando 'Ferma disegno'.",
+        "edit": "Modifica l'are trascinando i nodi.",
+        "save": "Sala area"
+      }
+    },
+    "buttons": {
+      "ok": "OK",
+      "cancel": "Annulla",
+      "finish": "Ferma disegna",
+      "save": "Salva",
+      "movePlaces": "Sposta e cancella i luoghi",
+      "deleteCategory": "Cancella",
+      "deleteCategoryAndPlaces": "Cancella categorie e luoghi",
+      "changeToPublic": "Rendi pubblico",
+      "changeToPrivate": "Rendi privato"
+    },
+    "placeform": {
+      "title": "Dati dei luoghi",
+      "tooltip": "I luoghi della mappa possono essere salvati in Miei Luoghi. Dai un nome al luogo e descrivilo. Puoi inserire un luogo in un layer esistente, oppure puoi creare un nuovo layer selezioando 'Nuovo layer' nel menu' della mappa.",
+      "placename": {
+        "placeholder": "Battezza il luogo"
+      },
+      "placelink": {
+        "placeholder": "Associa un URL"
+      },
+      "placedesc": {
+        "placeholder": "Descrivi il luogo"
+      },
+      "category": {
+        "label": "Layer",
+        "new": "Nuovo layer..."
+      }
+    },
+    "categoryform": {
+      "name": {
+        "label": "Nome",
+        "placeholder": "Battezza il layer"
+      },
+      "drawing": {
+        "label": "Stile",
+        "point": {
+          "label": "Punto",
+          "color": "Colore",
+          "size": "Dimensione"
+        },
+        "line": {
+          "label": "Linea",
+          "color": "Colore",
+          "size": "Spessore"
+        },
+        "area": {
+          "label": "Area",
+          "fillcolor": "Colore di riempimento",
+          "linecolor": "Colore del bordo",
+          "size": "Spessore del bordo"
+        }
+      },
+      "edit": {
+        "title": "Modifica il layer",
+        "save": "Salva",
+        "cancel": "Annulla"
+      }
+    },
+    "notification": {
+      "placeAdded": {
+        "title": "Il luogo e' stato salvato",
+        "message": "Il luogo puo' essere trovato nel menu' Miei Dati"
+      },
+      "categorySaved": {
+        "title": "Layer salvato",
+        "message": "Le modifiche al layer sono state salvate"
+      },
+      "categoryDelete": {
+        "title": "Cancella layer",
+        "deleteConfirmMove": "Il layer {0} contiene {1} oggetti. Vuoi cancellare questo layer e spostare i luoghi ivi contenuti nel layer di default {2} ?",
+        "deleteConfirm": "Vuoi cancellare il layer {0}?",
+        "deleted": "Layer cancellato"
+      },
+      "categoryToPublic": {
+        "title": "Rendi pubblico il layer",        
+        "message": "Stai per rendere pubblico il layer \"{0}\". Potrai condividere il link a questo layer con gli altri utenti, oppure inserirlo in una mappa di un altro sito. Gli altri utenti potranno visualizzare il layer su Paikkatietoikkuna."
+      },
+      "categoryToPrivate": {
+        "title": "Make map layer private",        
+        "message": "Stai per rendere privato il layer \"{0}\". Non sara' piu' possibile condividere il layer con altri utenti od inserirlo in mappe su altri siti. Gli altri untenti non potranno piu' visualizzare il layer su Paikkatietoikkuna."
+      },
+      "error": {
+        "addCategory": "Il layer non puo' essere salvato. I luoghi non sono stati ancora salvati.",
+        "editCategory": "Il layer non puo' essere salvato.",
+        "savePlace": "I luoghi non possono essere salvati.",
+        "title": "Errore!",
+        "generic": "Errore di sistema; riprova piu' tardi.",
+        "deleteCategory": "Errore durante la cancellazione!",
+        "deleteDefault": "Il layer di default non puo' essere cancellato."
+      }
+    },
+    "validation": {
+      "title": "I dati contengono errori:",
+      "placeName": "Manca il nome del luogo.",
+      "categoryName": "Manca il nome del layer.",
+      "placeNameIllegal": "Il nome dell'oggetto contiene caratteri non validi. I caratteri consentiti sono le lettere, le cifre, il sottolineato ed il trattino.",
+      "descIllegal": "La descrizione dell'oggett contiene caratteri non validi.  I caratteri consentiti sono le lettere, le cifre, il sottolineato ed il trattino.",
+      "categoryNameIllegal": "La descrizione del layer contiene carattere non validi.  I caratteri consentiti sono le lettere, le cifre, il sottolineato ed il trattino.",
+      "dotSize": "La dimensione del punto esula dall'intervallo previsto (1-50).",
+      "dotColor": "Colore del punto errato.",
+      "lineSize": "La dimensione della linea non rientra nell'intervallo previsto (1-50).",
+      "lineColor": "Colore della linea errato.",
+      "areaLineSize": "La dimensione del bordo non rientra nell'intervallo previsto (0-50).",
+      "areaLineColor": "Colore del bordo errato.",
+      "areaFillColor": "Colore del riempimento errato."
+    }
+  }
+});
