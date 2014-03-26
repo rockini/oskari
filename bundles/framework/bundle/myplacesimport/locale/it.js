46e4ea21cf81251cd105a2a79c8c75e0f4829718
claudio rocchini <claudio@rockini.name>
2014-03-26 14:05:51 +0100

    it translation from Istituto Geografico Militare

---
+++ bundles/framework/bundle/myplacesimport/locale/it.js
@@ -0,0 +1,56 @@
+Oskari.registerLocalization({
+    "lang": "it",
+    "key": "MyPlacesImport",
+    "value": {
+        "title": "Importazione dei miei dati",
+        "desc": "",
+        "tool": {
+            "tooltip": "Inserire i propri dati"
+        },
+        "flyout": {
+            "title": "Importazione dei miei dati",
+            "description": "E' possibile caricare i propri dati anche in formato zip.",
+            "actions": {
+                "cancel": "Annulla",
+                "next": "Successivo"
+            },
+            "file": {
+                "submit": "Invia"
+            },
+            "layer": {
+                "title": "Dati a livello di record:",
+                "name": "Nome",
+                "desc": "Descrizione",
+                "source": "URL",
+                "style": "Stile"
+            },
+            "validations": {
+                "error": {
+                    "title": "Errore",
+                    "message": "Nome del file mancante."
+                }
+            },
+            "finish": {
+                "success": {
+                    "title": "Caricamento eseguito",
+                    "message": "E' possibile trovare le informazioni sui dati caricati nel menu 'Miei dati'"
+                },
+                "failure": {
+                    "title": "Impossibile importare i dati; riprova piu' tardi."
+                }
+            }
+        },
+        "tab": {
+            "title": "Dati",
+            "grid": {
+                "name": "Nome",
+                "description": "Descrizione",
+                "source": "URL"
+            }
+        },
+        "layer": {
+            "organization": "Cosa nostra",
+            "inspire": "Cosa nostra"
+        }
+    }
+});
